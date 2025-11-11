a380e47e0ea8d5fdcdb7a6179908c860ef08ce22
sertacd81 <sertacd@gmail.com>
2025-11-11 22:31:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-// v0.4.3 – icon & splash integrated; force update
-const CACHE = 'balik-sansi-v13';
-const ASSETS = [
-  './','./index.html','./app.js?v=0.4.2','./manifest.webmanifest',
-  './icons/icon-192.png','./icons/icon-512.png',
-  './icons/favicon-32.png','./icons/favicon-64.png'
-];
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))).then(()=> self.clients.claim())
-  );
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE='balik-sansi-v14';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+'./','./index.html','./manifest.webmanifest',
+'./icons/apple-touch-icon-120.png','./icons/apple-touch-icon-152.png','./icons/apple-touch-icon-167.png','./icons/apple-touch-icon-180.png',
+'./icons/icon-192.png','./icons/icon-512.png','./icons/favicon-32.png','./icons/favicon-64.png'
+])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(k1=>caches.delete(k1)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
